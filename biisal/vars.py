f65c7c9a76fbea62672f420ca38df12c8b6f6643
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-07-13 23:43:55 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -34,6 +34,8 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    FQDN = "substantial-eryn-cinemicastreamer-a599f26c.koyeb.app"
+    HAS_SSL = True 
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
